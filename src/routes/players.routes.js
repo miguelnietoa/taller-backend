bbd4503dcc7803135362435d6c4bb37b9bc8de64
JhanU1 <49659395+JhanU1@users.noreply.github.com>
2022-03-11 00:37:56 -0500

    Endpoints jhan (#3)
    * Add files via upload
    
    * segundo punto resuelto
    
    * Add files via upload
    
    * Add rules
    
    * added rules
    
    * add rules
    
    Co-authored-by: Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>

---
+++ src/routes/players.routes.js
@@ -0,0 +1,97 @@
+import data from "../utils/data.js";
+import { Router } from "express";
+import { generateId } from '../utils/utils.js';
+
+const router = Router();
+
+
+router.get("/", (req, res) => {
+  res.status(200).json(data.players);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const player = data.players.find((player) => player.id == id);
+  if (player) {
+    res.status(200).json(player);
+  } else {
+    res.status(404).json({ message: "player not found" });
+  }
+});
+
+router.get("/:id/characters", (req, res) => {
+  const { id } = req.params;
+  const player = data.players.find((player) => player.id === id);
+  if (player) {
+    const characters = data.playerCharacters.filter((playerCharacter) => playerCharacter.player === id);
+    res.status(200).json(characters);
+  } else {
+    res.status(404).json({ message: "Player not found" });
+  }
+});
+
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  const player = data.players.find((player) => player.username === username && player.password === password);
+  if (player) {
+    player.last_login = new Date();
+    res.status(200).json({ logged: true });
+  } else {
+    res.status(404).json({ message: "Invalid credentials" });
+  }
+});
+
+router.post("/", (req, res) => {
+  const { name, password, username } = req.body;
+  if (!name || !password || !username) {
+    res.status(400).json({ message: "Please provide playername and name" });
+  } else {
+    const newplayer = {
+      id: generateId(data.players),
+      name,
+      last_login: new Date(),
+      password,
+      username
+    };
+    data.players.push(newplayer);
+    res.status(201).json(newplayer);
+  }
+});
+
+router.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, last_login, password, username } = req.body;
+  const player = data.players.find((player) => player.id == id);
+  if (player) {
+    if (name) {
+      player.name = name;
+    }
+    if (last_login) {
+      player.last_login = last_login;
+    }
+    if (password) {
+      player.password = password;
+    }
+    if (username) {
+      player.username = username;
+    }
+    res.status(200).json(player);
+  } else {
+    res.status(404).json({ message: "player not found" });
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const player = data.players.find((player) => player.id == id);
+  if (player) {
+    const index = data.players.indexOf(player);
+    data.players.splice(index, 1);
+    res.status(200).json(player);
+  } else {
+    res.status(404).json({ message: "player not found" });
+  }
+});
+
+
+export default router;
